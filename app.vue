5b029b9cda75bce076988c3710f1b01130d7c493
Jamshid Abduqodirov <jamshidabdukadirov01@gmail.com>
2025-02-11 15:04:09 +0500

    Fixing UI

--- app.vue
+++ app.vue
@@ -1,6 +1,13 @@
+<script setup>
+  const { data } = await useFetch("/main/Category/GetAll");
+
+  onMounted(async () => {
+    const reponse = await $fetch("/main/Category/GetAll");
+    console.log(reponse);
+  });
+</script>
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <pre v-if="data" class="container mx-auto">
+      {{ data }}
+  </pre>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
-})
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/i18n"],
+  app: {
+    head: {
+      title: "Mening tomorqam",
+    },
+  },
+  routeRules: {
+    "/main/**": {
+      proxy: {
+        to: "https://4ec1-93-188-80-228.ngrok-free.app/**",
+        headers: { "ngrok-skip-browser-warning": "69420" },
+      },
+    },
+  },
+});
