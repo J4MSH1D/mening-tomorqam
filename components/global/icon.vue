b6e0fdc90002fa78fd9b4d084db2f97d5fd2cd14
Alisher1403 <alisherchinberdiyev1403@gmail.com>
2025-02-12 12:40:03 +0500

    Fixing icon

--- components/global/icon.vue
+++ components/global/icon.vue
@@ -23,11 +23,12 @@ if (props.isSvgRaw) {
   svgRawPath.value = res.default;
 }
 
-const pathOfImage = computed(async () => {
+const pathOfImage = computed(() => {
   if (props.isSvg) return `/images/svg/${props.name}.svg`;
   else return `/images/png/${props.name}`;
 });
 </script>
+
 <template>
   <div :alt="name" v-if="isSvgRaw" v-html="svgRawPath" />
   <img :src="pathOfImage" :alt="name" v-else />
